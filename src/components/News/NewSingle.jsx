b9dd48fac284fbc3d43cc3768af8082f8a73d0a3
trish77 <trish.dev7@gmail.com>
2018-11-18 13:31:21 -0600

    first time news app

---
+++ src/components/News/NewSingle.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const NewSingle = ({item}) => (
+      <div className="card col-3 mb-5">
+        <img className="card-img-top" src={item.urlToImage} alt="{item.title}" />
+        <div className="card-body">
+          <h5 className="card-title">{item.source.name}</h5>
+          <p className="card-text">{item.content}</p>
+        
+        </div>
+          <div className="card-footer">
+          <p className="card-text">  <a href={item.url} target="_blank">Full Article</a></p>
+          </div>
+      </div>
+);
+
+export default NewSingle;
---
+++ src/components/News/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NewSingle from "./NewSingle";
+
+class News extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			news: []
+		};
+	}
+
+	componentDidMount() {
+		const url =
+			"https://newsapi.org/v2/everything?q=bitcoin&from=2018-10-18&sortBy=publishedAt&apiKey=b24a46973f1a441c8926e85311686acd";
+
+		fetch(url)
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				this.setState({
+					news: data.articles
+				});
+			})
+			.catch(error => console.log(error));
+	}
+
+	renderItems() {
+		return this.state.news.map(item => <NewSingle key={item.url} item={item} />);
+	}
+
+	render() {
+		return (
+		<div className="container">
+		<div className="row mb-4">
+
+				{this.renderItems()}
+
+		</div>
+		</div>
+		);
+	}
+}
+
+export default News;
